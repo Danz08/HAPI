eb8d980886d1cd0f74c0eb4bf63bb9c66a961e4c
Danz08 <ganzmobaid@gmail.com>
2026-05-28 19:58:07 +0700

    fixxxx

--- server.js
+++ server.js
@@ -38,6 +38,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('trust proxy', 1);
+
 app.use(session({
   store: new pgSession({
     pool: pool,
